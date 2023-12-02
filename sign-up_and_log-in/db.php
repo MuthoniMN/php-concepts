b151b50c10374a2c9fda16fc6e0963bd01030864
Michelle Ndiangui <ndianguimichelle@gmail.com>
2023-12-02 13:57:11 +0300

    adding homepage

--- sign-up_and_log-in/db.php
+++ sign-up_and_log-in/db.php
@@ -6,8 +6,6 @@ $database = 'users';
 
 $connection = mysqli_connect($host, $username, $password, $database);
 
-if ($connection) {
-    echo "Connected to the database";
-} else {
+if (!$connection) {
     echo "Connection error";
 }
